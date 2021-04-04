22a0d91cceed697f758803e8d1e79813623e98d6
Peter <varga.peter826@gmail.com>
2021-04-04 20:33:32 +0200

    README and plot.py update

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # DL-Project-2
 Repo for the Deep Learning Project 2, Group 10
+
+## How to RUN (RNN models):
+- Place the 'data' folder within the RNN folder
+- Running an individual experiment:
+    * open the terminal within RNN/ and specify the desired configuration with the arguments:
+        * python3 run.py -M train -E MogLSTM -ED 2 -D MogLSTM -O ADAM -EN 100 -lr 0.001
+        * outputs (within 'txt_results' folder):
+            - bleu_train_GRU2GRU_d0.1_gc1.0_lr0.001.txt
+            - loss_train_GRU2GRU_d0.1_gc1.0_lr0.001.txt
+- Running a bunch of experiments:
+    * open the terminal within RNN/ and run one of the bash scripts (e.g. ./train_GRU_clip.sh)
+
+## How to visualize experiment results (RNN models):
+- open the terminal within RNN/ and run plot.py accompanied by the following arguments:
+    * the datafile,
+    * and the metric with the modified (relative to default settings) hyperparameters in the experiment
+- Example: python3 plot.py -p "txt_results/bleu_train_GRU2GRU_d0.1_gc1.0_lr0.001.txt" -n "bleu_gc1.0"
+- output: GRU2GRU_bleu_gc1.0.png
--- RNN/plot.py
+++ RNN/plot.py
@@ -3,126 +3,58 @@
 import os
 import matplotlib.pyplot as plt
 import re
+import argparse
 from typing import List
 
 
 def read_data(path):
     file = os.path.abspath(f".\\{path}")
     model_name = re.findall(r'[A-Z]+\d[A-Z]+', os.path.basename(file))[0]
+    metric = os.path.basename(file).split("_")[0]
     with open(file, "r") as file_obj:
         data = file_obj.readlines()
     data = [float(data.split("\n")[0]) for data in data]
-    return model_name, data
+    return model_name, data, metric
 
 
-def plot_cross_entropy(losses: List[list], seq2seq_model: str, file_name=None):
+def plot_results(seq2seq_model: str, data: list, metric: str, file_ext=None):
     """ Plotting function for X number of epochs,
-     where each X is a single value - the average loss over the given epoch
+     where each X is a single value - the average loss/bleu score over the given epoch
     """
     fs = 20
     figure = plt.figure(figsize=(16, 12))
-    for loss in losses:
-        plt.plot(loss)
-    plt.legend(["Training loss", "Validation loss"], fontsize=fs)
-    plt.ylabel("Cross-Entropy Loss", fontsize=fs)
-    plt.xlabel("epoch", fontsize=fs)
-    plt.title(f"Performance of {seq2seq_model}", fontsize=fs)
-    plt.yticks(fontsize=fs)
-    plt.xticks(fontsize=fs)
-    figure.savefig(f"{seq2seq_model}_{file_name}.png")
-
-
-def plot_bleu_score(scores: List[list], seq2seq_model: str, file_name=None):
-    """ Plotting function for X number of epochs,
-     where each X is a single value - the average bleu score the given epoch
-    """
-    fs = 20
-    figure = plt.figure(figsize=(16, 12))
-    for score in scores:
-        plt.plot(score)
-    plt.legend(["Training score", "Validation score"], fontsize=fs)
-    plt.ylabel("BLEU Score", fontsize=fs)
-    plt.xlabel("epoch", fontsize=fs)
-    plt.title(f"Individual 1-Gram BLEU score of {seq2seq_model}", fontsize=fs)
-    plt.yticks(fontsize=fs)
-    plt.xticks(fontsize=fs)
-    figure.savefig(f"{seq2seq_model}_{file_name}.png")
-
-
-def read_and_plot(train_path, val_path, file_ext, metric):
-    model_name, train_data = read_data(train_path)
-    _, val_data = read_data(val_path)
-    if metric == "cross-entropy":
-        plot_cross_entropy([train_data, val_data], model_name, file_ext)
-    elif metric == "bleu":
-        plot_bleu_score([train_data, val_data], model_name, file_ext)
+    plt.plot(data)
+    if metric == "loss":
+        plt.ylabel("Cross-Entropy Loss", fontsize=fs)
+        plt.title(f"Performance of {seq2seq_model}", fontsize=fs)
+        plt.legend(["Training loss"], fontsize=fs)
     else:
-        raise ValueError("Wrong metric parameter was given. Possible options: ['cross-entropy', 'bleu']")
+        plt.ylabel("BLEU Score", fontsize=fs)
+        plt.title(f"Individual 1-Gram BLEU score of {seq2seq_model}", fontsize=fs)
+        plt.legend(["Training score"], fontsize=fs)
+    plt.xlabel("epoch", fontsize=fs)
+    plt.yticks(fontsize=fs)
+    plt.xticks(fontsize=fs)
+    figure.savefig(f"{seq2seq_model}_{file_ext}.png")
 
 
-os.chdir("txt_results\\Exp set - joint voc, hidden 300")
+def read_and_plot(train_path, file_ext):
+    model_name, train_data, metric = read_data(train_path)
+    plot_results(model_name, train_data, metric, file_ext)
 
-# -------------------------------------------------- GRU --------------------------------------------------
-# ------- Gradient clipping -------
-read_and_plot("loss_train_GRU2GRU_d0.1_gc1.0_lr0.0001.txt",
-              "loss_val_GRU2GRU_d0.1_gc1.0_lr0.0001.txt",
-              "loss_gc1.0", "cross-entropy")
 
-read_and_plot("train_GRU2GRU_d0.1_gc1.0_lr0.0001.txt",
-              "val_GRU2GRU_d0.1_gc2.0_lr0.0001.txt",
-              "gc2.0", "cross-entropy")
+# ---- Example uses of 'read_and_plot' -----
+# read_and_plot("loss_train_GRU2GRU_d0.1_gc1.0_lr0.001.txt", "loss_gc1.0")
+# read_and_plot("bleu_train_GRU2GRU_d0.1_gc1.0_lr0.001.txt", "bleu_gc1.0")
 
-# ------- DROPOUT -------
-read_and_plot("train_GRU2GRU_d0.1_gc50.0_lr0.0001.txt",
-              "val_GRU2GRU_d0.1_gc50.0_lr0.0001.txt",
-              "d0.1", "cross-entropy")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', help="Path to txt file containing the experiment data",
+                        type=str, default=None)
+    parser.add_argument('-n', '--name', help="Additional string to the filename,"
+                                             " marking the experiment type and values")
+    args = vars(parser.parse_args())
+    read_and_plot(args.get("path"), args.get("name"))
 
-read_and_plot("train_GRU2GRU_d0.2_gc50.0_lr0.0001.txt",
-              "val_GRU2GRU_d0.2_gc50.0_lr0.0001.txt",
-              "d0.2", "cross-entropy")
 
-read_and_plot("train_GRU2GRU_d0.3_gc50.0_lr0.0001.txt",
-              "val_GRU2GRU_d0.3_gc50.0_lr0.0001.txt",
-              "d0.3", "cross-entropy")
-
-# ------- Learning rate -------
-read_and_plot("train_GRU2GRU_d0.1_gc50.0_lr0.0001.txt",
-              "val_GRU2GRU_d0.1_gc50.0_lr0.0001.txt",
-              "lr0.0001", "cross-entropy")
-
-read_and_plot("train_GRU2GRU_d0.1_gc50.0_lr0.001.txt",
-              "val_GRU2GRU_d0.1_gc50.0_lr0.001.txt",
-              "lr0.001", "cross-entropy")
-
-# -------------------------------------------------- LSTM --------------------------------------------------
-# ------- Gradient clipping -------
-read_and_plot("train_LSTM2LSTM_d0.1_gc1.0_lr0.0001.txt",
-              "val_LSTM2LSTM_d0.1_gc1.0_lr0.0001.txt",
-              "gc1.0", "cross-entropy")
-
-read_and_plot("train_LSTM2LSTM_d0.1_gc1.0_lr0.0001.txt",
-              "val_LSTM2LSTM_d0.1_gc2.0_lr0.0001.txt",
-              "gc2.0", "cross-entropy")
-
-# ------- DROPOUT -------
-# read_and_plot("train_LSTM2LSTM_d0.1_gc50.0_lr0.0001.txt",
-#               "val_LSTM2LSTM_d0.1_gc50.0_lr0.0001.txt",
-#               "d0.1", "cross-entropy)
-#
-# read_and_plot("train_LSTM2LSTM_d0.2_gc50.0_lr0.0001.txt",
-#               "val_LSTM2LSTM_d0.2_gc50.0_lr0.0001.txt",
-#               "d0.2", "cross-entropy)
-#
-# read_and_plot("train_LSTM2LSTM_d0.3_gc50.0_lr0.0001.txt",
-#               "val_LSTM2LSTM_d0.3_gc50.0_lr0.0001.txt",
-#               "d0.3", "cross-entropy)
-
-# ------- Learning rate -------
-read_and_plot("train_LSTM2LSTM_d0.1_gc50.0_lr0.0001.txt",
-              "val_LSTM2LSTM_d0.1_gc50.0_lr0.0001.txt",
-              "lr0.0001", "cross-entropy")
-
-read_and_plot("train_LSTM2LSTM_d0.1_gc50.0_lr0.001.txt",
-              "val_LSTM2LSTM_d0.1_gc50.0_lr0.001.txt",
-              "lr0.001", "cross-entropy")
 
